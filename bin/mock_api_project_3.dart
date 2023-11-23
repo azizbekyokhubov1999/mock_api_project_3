cc53f622ba3a6dac6dafac6918855e32fd2b39a0
Azizbek <azizbek.yoqubov.99@gmail.com>
2023-11-23 15:49:28 +0300

    api3_commit 3

---
+++ bin/mock_api_project_3.dart
@@ -0,0 +1,5 @@
+import 'package:mock_api_project_3/mock_api_project_3.dart' as mock_api_project_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mock_api_project_3.calculate()}!');
+}
